856457207a432c3d3cdf9297977580420a3eb73b
Fingolf6455 <72171703+Fingolf6455@users.noreply.github.com>
2023-03-13 23:50:37 +0100

    Add files via upload

---
+++ Hand-in/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask('Storyforge')
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('Story-forge.html')
+
+# About page
+@app.route('/about')
+def about():
+    return render_template('Story-Forge-About.html')
+
+# Contact page
+@app.route('/contact')
+def contact():
+    return render_template('Story-Forge-Contact.html')
+
+# Layout page
+@app.route('/Layout')
+def layout():
+    return render_template('Layout.html')
+
+# Dynamic page that displays a number
+@app.route('/display_number/<int:number>')
+def display_number(number):
+    return render_template('dynamic.html', number=number)
+
+# Dynamic page that displays a user's profile
+@app.route('/dynamic', methods=['GET', 'POST'])
+def dynamic():
+    if request.method == 'POST':
+        number = request.form['number']
+        return redirect(url_for('display_number', number=number))
+    return render_template('dynamic.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
